4b8b3a5132ad3a6f6b2076704faa12445d699135
André Meriac <62508688+a-meriac@users.noreply.github.com>
2025-03-21 09:59:02 +0400

    Added city class and imported csv as list

--- main.py
+++ main.py
@@ -1,9 +1,26 @@
 import csv
 
-with open('names.csv', newline='') as csvfile:
+cities = []
 
-    reader = csv.DictReader(csvfile)
+def check_city(city):
+    if city in cities:
+        return True
+    else:
+        return False
+
+class City:
+  def __init__(self, name, latitude, longitude ):
+    self.name = name
+    self.age = age
+    self.latitude = latitude
+    self.longitude = longitude
+
+with open('worldcities.csv', 'r', encoding='utf-8') as f:
+    reader = csv.reader(f)
+
+    next(reader)
 
     for row in reader:
+        cities.append(row[0])
 
-        print(row['first_name'], row['last_name'])
+    print(cities)
