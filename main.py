806c1566a01df8f07252b9ce0ab689982423ac35
André Meriac <62508688+a-meriac@users.noreply.github.com>
2025-03-21 11:04:39 +0400

    Added make_distance_list()

--- main.py
+++ main.py
@@ -13,23 +13,37 @@ def enter_city():
         print('City not found, please try again and ensure proper punctuation and capitalization')
         return enter_city()
 
-def distance(lat1, lon1, lat2, lon2):
-    # Convert latitude and longitude from degrees to radians
+def distance(index1, index2):
+    lat1 = latitudes[index1]
+    lon1 = longitudes[index1]
+    lat2 = latitudes[index2]
+    lon2 = longitudes[index2]
+
     lat1 = math.radians(float(lat1))
     lon1 = math.radians(float(lon1))
     lat2 = math.radians(float(lat2))
     lon2 = math.radians(float(lon2))
 
-    # Haversine formula
     dlat = lat2 - lat1
     dlon = lon2 - lon1
 
     a = math.sin(dlat / 2)**2 + math.cos(lat1) * math.cos(lat2) * math.sin(dlon / 2)**2
     c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
 
-    # Earth's radius in kilometers (mean radius)
     circumference = 6371.0
     return circumference * c
+
+
+def make_distance_list(city_list):
+    distance_list = [[0 for _ in range(6)] for _ in range(6)]
+
+    for i in range(len(city_list)):
+        for j in range(len(city_list)):
+            distance_list[i][j] = distance(city_list[i], city_list[j])
+
+    return distance_list
+
+
 
 class City:
   def __init__(self, name, latitude, longitude ):
@@ -59,7 +73,7 @@ def main():
 
 """)
     print('Welcome to City Route!')
-    print('\nPlease enter 5 cities starting with your starting city\n')
+    print('\nPlease enter 5 cities:\n')
     c1_index = enter_city()
     c1 = City(cities[c1_index], latitudes[c1_index], longitudes[c1_index])
 
@@ -75,11 +89,15 @@ def main():
     c5_index = enter_city()
     c5 = City(cities[c5_index], latitudes[c5_index], longitudes[c5_index])
 
-    print(c1.name, c2.name, c3.name, c4.name, c5.name)
-    print(c1.latitude, c2.latitude, c3.latitude, c4.latitude, c5.latitude)
-    print(c1.longitude, c2.longitude, c3.longitude, c4.longitude, c5.longitude)
+    start = City("Start", float(input("\n Please enter the latitude of your starting point:")), float(input("\n Please enter the longitude of your starting point:")))
 
-    for x in range(5):
+    city_list = [start, c1, c2, c3, c4, c5]
+    distance_list = make_distance_list(city_list)
+
+    print(city_list)
+    print(distance_list)
+
+
 
 
 
